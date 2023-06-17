00f3c034e7d76e16d21c23558c8ce3cb664eec2c
Antonio Sanjuan <tonysanj13@gmail.com>
2023-06-18 00:49:49 +0200

    feature: added service

--- src/app/services/user-service/user-service.service.ts
+++ src/app/services/user-service/user-service.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 import { IUserInfo } from 'src/app/models/user-info.model';
+import { IUser } from 'src/app/models/dtos/user.model';
+import { ICity } from 'src/app/models/dtos/city.model';
 
 @Injectable()
 export class UsersService {
@@ -12,5 +14,23 @@ export class UsersService {
 
   public getListOfUsers(): Observable<IUserInfo[]> {
     return this.http.get<IUserInfo[]>(`https://6390b47b65ff4183111c4b91.mockapi.io/users/users`)
+  }
+
+  private getListOfUsers2(): Observable<IUser[]> {
+    return this.http.get<IUser[]>(`https://646b8fc77d3c1cae4ce3ffe0.mockapi.io/commonapi/users`)
+  }
+
+  private getListOfCities(): Observable<ICity[]> {
+    return this.http.get<ICity[]>(`https://646b8fc77d3c1cae4ce3ffe0.mockapi.io/commonapi/cities`)
+  }
+
+  public getListOfUsersAndCities(): Observable<{
+    users: IUser[];
+    cities: ICity[];
+  }> {
+    return forkJoin({
+      users: this.getListOfUsers2(),
+      cities: this.getListOfCities()
+    })
   }
 }
