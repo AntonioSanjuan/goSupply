542b476140c314f27a8e62aa71af505d91ff4753
Antonio Sanjuan <tonysanj13@gmail.com>
2023-06-17 23:18:57 +0200

    feature: logged userInfo changes

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { IUserInfo, UserInfo } from 'src/app/models/user-info.model';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
-  styleUrls: ['./user-info.component.scss']
+  styleUrls: ['./user-info.component.scss'],
 })
-export class UserInfoComponent {
+export class UserInfoComponent implements OnChanges{
   @Input() userInfo!: IUserInfo
+
+  ngOnChanges() {
+    this.loguserInfoChanges()
+  }
+
+  private loguserInfoChanges() {
+    console.log("userInfo", this.userInfo)
+  }
 }
