8e4d87cd0170b68b3e807b7da3368fc49531f890
Antonio Sanjuan <tonysanj13@gmail.com>
2023-06-17 23:27:41 +0200

    feature: added list-of-users component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { UserSettingsComponent } from './components/user-settings/user-settings.component';
 import { UserInfoComponent } from './components/user-info/user-info.component';
 import { FormsModule } from '@angular/forms';
+import { ListOfUsersComponent } from './components/list-of-users/list-of-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserSettingsComponent,
-    UserInfoComponent
+    UserInfoComponent,
+    ListOfUsersComponent
   ],
   imports: [
     BrowserModule,
