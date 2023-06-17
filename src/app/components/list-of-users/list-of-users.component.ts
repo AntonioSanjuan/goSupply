5191a394821ab132e8efaf07dd0d2e10cc0859d7
Antonio Sanjuan <tonysanj13@gmail.com>
2023-06-18 00:53:44 +0200

    feature: both request at the same time

--- src/app/components/list-of-users/list-of-users.component.ts
+++ src/app/components/list-of-users/list-of-users.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IUserInfo } from 'src/app/models/user-info.model';
 import { UsersService } from 'src/app/services/user-service/user-service.service';
-import {Observable} from 'rxjs'
+import {Observable, Subscription} from 'rxjs'
 @Component({
   selector: 'app-list-of-users',
   templateUrl: './list-of-users.component.html',
   styleUrls: ['./list-of-users.component.scss']
 })
-export class ListOfUsersComponent implements OnInit{
+export class ListOfUsersComponent implements OnInit, OnDestroy{
   public listOfUsers$!: Observable<IUserInfo[]>
+  private subscriptions: Subscription[] = []
   constructor(public userService: UsersService) {}
 
   ngOnInit(): void {
     this.listOfUsers$ = this.userService.getListOfUsers()
+
+    this.subscriptions.push(
+      this.userService.getListOfUsersAndCities().subscribe((data) => {
+        console.log("data", data)
+      })
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => {
+      subscription.unsubscribe();
+    })
   }
 }
