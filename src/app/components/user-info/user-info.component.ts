944224ffebb74f775893001dd4617ea5ad6b45e5
Antonio Sanjuan <tonysanj13@gmail.com>
2023-06-17 23:05:56 +0200

    feature: added some functs to user-info component

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { UserInfo } from 'src/app/models/user-info.model';
+import { Component, Input } from '@angular/core';
+import { IUserInfo, UserInfo } from 'src/app/models/user-info.model';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
-  styleUrls: ['./user-info.component.css']
+  styleUrls: ['./user-info.component.scss']
 })
 export class UserInfoComponent {
+  @Input() userInfo!: IUserInfo
 }
